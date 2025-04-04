5f1948631f57eaeffd72764b7f5fa680bd361a8d
Alfredo Catzin <alfredocatzin10@me.com>
2025-04-03 18:05:02 -0700

    Changes to CentalShoppingSystem class as well as amendment to previous commit

--- coderedecommerce/src/main/java/com/codered/ecomerce/model/CentralShoppingSystem.java
+++ coderedecommerce/src/main/java/com/codered/ecomerce/model/CentralShoppingSystem.java
@@ -1,6 +1,29 @@
 package com.codered.ecomerce.model;
 
+import com.codered.ecommerce.model.Customer;
+import com.codred.ecommerce.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 public class CentralShoppingSystem 
 {
+    private List<Customer> customers;
+    private List<Product> products;
+
+    //Constructor
+    public CentralShoppingSystem()
+    {
+        customers = new ArrayList<>();
+        products = new ArrayList<>();
+    }
+
+    public void initializeSystem()
+    {
         
+    }
+
+    public void displaySystemInfo()
+    {
+
+    }
 }
