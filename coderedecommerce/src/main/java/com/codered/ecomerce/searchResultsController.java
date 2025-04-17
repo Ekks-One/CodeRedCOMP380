01809498c1fdd7bdd3ad7a67f6c3638cd822a2bc
Ekks <xavier.ramos.835@my.csun.edu>
2025-04-17 01:36:34 -0700

    Comments on searchResults populateGridPane. Commented out Variants testing.

--- coderedecommerce/src/main/java/com/codered/ecomerce/searchResultsController.java
+++ coderedecommerce/src/main/java/com/codered/ecomerce/searchResultsController.java
@@ -43,84 +43,9 @@ public class searchResultsController {
     @FXML private TextField searchTextBox;
     @FXML private GridPane searchGridPane;
 
-    public void initialize() {
-    ArrayList<Product> products = CentralShoppingSystem.getProducts();
-    List<Variant> variants = new ArrayList<>();
-
-        variants.add(new Variant(1, Color.RED, Material.COTTON, Size.S, 50, 19.99));
-        variants.add(new Variant(2, Color.BLUE, Material.POLYESTER, Size.M, 30, 24.99));
-        variants.add(new Variant(3, Color.BLACK, Material.DENIM, Size.L, 40, 49.99));
-        variants.add(new Variant(4, Color.WHITE, Material.COTTON, Size.XL, 25, 29.99));
-        variants.add(new Variant(5, Color.GREEN, Material.LINEN, Size.M, 60, 34.99));
-        variants.add(new Variant(6, Color.YELLOW, Material.SILK, Size.S, 20, 39.99));
-        variants.add(new Variant(7, Color.GREY, Material.WOOL, Size.L, 70, 59.99));
-        variants.add(new Variant(8, Color.BROWN, Material.LEATHER, Size.XL, 10, 89.99));
-        variants.add(new Variant(9, Color.PURPLE, Material.COTTON, Size.S, 35, 21.99));
-        variants.add(new Variant(10, Color.ORANGE, Material.POLYESTER, Size.M, 80, 27.99));
-    
-
-    int row = 0;
-    int col = 0;
-    int maxCols = 4;
-    int prodCount = 0;
-    
-    for (Product product: products) {
-        if (product == null) {
-            System.out.println("Null product found! Skipping...");
-            continue;
-        }
-
-        if(prodCount >= 24) {
-            break;
-        }
-    
-    AnchorPane productPane = new AnchorPane();
-    productPane.setStyle("-fx-border-color: black; -fx-padding: 10 10 10 10;");
-
-    //Create ImageView for the product image
-    ImageView productImageView = new ImageView();
-    productImageView.setImage(new Image(getClass().getResource("/com/codered/ecomerce/images/Item 0-1.png").toExternalForm()));
-    productImageView.setFitWidth(100);
-    productImageView.setFitHeight(100);
-    productImageView.setPreserveRatio(true);
-    AnchorPane.setTopAnchor(productImageView, 10.0);
-    AnchorPane.setLeftAnchor(productImageView, 10.0);
-
-    Button addCartButton = new Button("Add to Cart");
-    AnchorPane.setTopAnchor(addCartButton, 120.0);
-    AnchorPane.setLeftAnchor(addCartButton, 10.0);
-    addCartButton.setOnAction(event -> {
-        // Add to cart logic here
-        System.out.println("Added " + product.getName() + " to cart.");
-    });
-
-
-    //Create Label for the product name
-    Label nameLabel = new Label(product.getName());
-    AnchorPane.setTopAnchor(nameLabel,10.0);
-    AnchorPane.setLeftAnchor(nameLabel, 120.0);
-
-    Label priceLabel = new Label("$" + product.getBasePrice());
-    priceLabel.setStyle("-fx-font-size: 12px; -fx-text-fill: green;");
-    AnchorPane.setTopAnchor(priceLabel,40.0);
-    AnchorPane.setLeftAnchor(priceLabel, 120.0);
-
-    productPane.getChildren().addAll(productImageView, nameLabel, priceLabel, addCartButton);
-
-    searchGridPane.add(productPane, col, row);
-    
-    col++;
-    if(col>= maxCols) {
-        col = 0;
-        row++;
+    public void initialize() throws IOException {
+        populateGridPane();
     }
-
-    prodCount++;
-    
-}
-
-
-}
 
 
     /**
@@ -148,6 +73,107 @@ public class searchResultsController {
         System.out.println("Returning to homepage...");
     }
 
+
+    /**
+     * Method to populate the grid pane with product items from the database
+     * @throws IOException if there is an error loading the fxml file
+     */
+
+    public void populateGridPane() throws IOException {
+        ArrayList<Product> products = CentralShoppingSystem.getProducts();
+        //List<Variant> variants = new ArrayList<>();
+        
+        //Added as a Test to figure out Variant situation
+        
+        //     variants.add(new Variant(1, Color.RED, Material.COTTON, Size.S, 50, 19.99));
+        //     variants.add(new Variant(2, Color.BLUE, Material.POLYESTER, Size.M, 30, 24.99));
+        //     variants.add(new Variant(3, Color.BLACK, Material.DENIM, Size.L, 40, 49.99));
+        //     variants.add(new Variant(4, Color.WHITE, Material.COTTON, Size.XL, 25, 29.99));
+        //     variants.add(new Variant(5, Color.GREEN, Material.LINEN, Size.M, 60, 34.99));
+        //     variants.add(new Variant(6, Color.YELLOW, Material.SILK, Size.S, 20, 39.99));
+        //     variants.add(new Variant(7, Color.GREY, Material.WOOL, Size.L, 70, 59.99));
+        //     variants.add(new Variant(8, Color.BROWN, Material.LEATHER, Size.XL, 10, 89.99));
+        //     variants.add(new Variant(9, Color.PURPLE, Material.COTTON, Size.S, 35, 21.99));
+        //     variants.add(new Variant(10, Color.ORANGE, Material.POLYESTER, Size.M, 80, 27.99));
+            
+        // for (Variant variant: variants) {
+        //     if (variant == null) {
+        //         System.out.println("Null product found! Skipping...");
+        //         continue;
+        //     }
+            
+
+        int row = 0;
+        int col = 0;
+        int maxCols = 4;
+        int prodCount = 0;
+        
+        // Loop through the products and create a new AnchorPane for each product
+        for(Product product : products) {
+            if(product == null) {
+                System.out.println("Null product found! Skipping...");
+                continue;
+            }
+
+            // Limit the number of products displayed to 24
+            if(prodCount >= 24) {
+                break;
+            }
+        
+        // Create a new AnchorPane for each product
+        AnchorPane productPane = new AnchorPane();
+        productPane.setStyle("-fx-border-color: black; -fx-padding: 10 10 10 10;");
+
+        //Create ImageView for the product image
+        ImageView productImageView = new ImageView();
+        productImageView.setImage(new Image(getClass().getResource("/com/codered/ecomerce/images/Item 0-1.png").toExternalForm()));
+        productImageView.setFitWidth(100);
+        productImageView.setFitHeight(100);
+        productImageView.setPreserveRatio(true);
+        AnchorPane.setTopAnchor(productImageView, 10.0);
+        AnchorPane.setLeftAnchor(productImageView, 10.0);
+
+        // Create Button for adding to cart
+        Button addCartButton = new Button("Add to Cart");
+        AnchorPane.setTopAnchor(addCartButton, 120.0);
+        AnchorPane.setLeftAnchor(addCartButton, 10.0);
+        addCartButton.setOnAction(event -> {
+            // Add the product to the cart
+
+            // To be added once Variants Situation is figured out: products.get(variant.getID()).getName()
+            System.out.println("Added " + product.getName() + " to cart.");
+        });
+
+
+        // Create Label for the product name
+        // To be added once Variants Situation is figured out: products.get(variant.getID()).getName()
+        Label nameLabel = new Label(product.getName());
+        AnchorPane.setTopAnchor(nameLabel,10.0);
+        AnchorPane.setLeftAnchor(nameLabel, 120.0);
+
+        // To be added once Variants Situation is figured out: variant.getPrice()
+        Label priceLabel = new Label("$" + product.getBasePrice());
+        priceLabel.setStyle("-fx-font-size: 12px; -fx-text-fill: green;");
+        AnchorPane.setTopAnchor(priceLabel,40.0);
+        AnchorPane.setLeftAnchor(priceLabel, 120.0);
+
+        // Add all elements to the product pane
+        productPane.getChildren().addAll(productImageView, nameLabel, priceLabel, addCartButton);
+
+        // Add the product pane to the grid
+        searchGridPane.add(productPane, col, row);
+        
+        // Update the row and column for the next product
+        col++;
+        if(col>= maxCols) {
+            col = 0;
+            row++;
+        }
+        //Increment the product count
+        prodCount++;
+
+        }
+    }
     /**
      * method to perform search from the search bar when the search button is clicked.
      * This method retrieves the text from the searchTextBox and prints it to the console.
