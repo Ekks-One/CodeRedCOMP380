f4e4a69fa951d07f59a019ed4a543f0c18c2675d
Miguel Alfaro <miguel.alfaroconcha.213@my.csun.edu>
2025-04-29 19:01:12 -0700

    Created test scaffolds for PaymentView/Primary Controllers and CartManager/Product Modules

---
+++ coderedecommerce/src/test/java/com/codered/ecomerce/PaymentViewControllerTest.java
@@ -0,0 +1,10 @@
+package com.codered.ecomerce;
+
+import org.junit.jupiter.api.Test;
+
+public class PaymentViewControllerTest {
+    @Test
+    void testConfirmPayment() {
+
+    }
+}
---
+++ coderedecommerce/src/test/java/com/codered/ecomerce/PrimaryControllerTest.java
@@ -0,0 +1,10 @@
+package com.codered.ecomerce;
+
+import org.junit.jupiter.api.Test;
+
+public class PrimaryControllerTest {
+    @Test
+    void testSearch() {
+
+    }
+}
